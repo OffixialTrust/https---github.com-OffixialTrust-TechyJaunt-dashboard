208f4715e265a4b0e461b03cf1767258d871eae9
Offixialtrust <richardtrust12@gmail.com>
2025-01-28 19:29:31 +0100

    Duplication Is Evil!

--- dashboard.js
+++ dashboard.js
@@ -17,107 +17,46 @@ function info(index, time, period) {
     return data[index].timeframes[time][period];
 }
 
+function changeData(timeframe, previousTime) {
+    className(".timeframe").forEach((element) => {
+        element.textContent = previousTime;  
+       });
+       
+       // for work
+       id("work-current").textContent = info(0, timeframe, "current");
+       id("work-previous").textContent = info(0, timeframe, "previous");
+       
+       // for play
+       id("play-current").textContent = info(1, timeframe, "current");
+       id("play-previous").textContent = info(1, timeframe, "previous");
+       
+       // for study
+       id("study-current").textContent = info(2, timeframe, "current");
+       id("study-previous").textContent = info(2, timeframe, "previous");
+       
+       // for exercise
+       id("exercise-current").textContent = info(3, timeframe, "current");
+       id("exercise-previous").textContent = info(3, timeframe, "previous");;
+       
+       // for social
+       id("social-current").textContent = info(4, timeframe, "current");
+       id("social-previous").textContent = info(4, timeframe, "previous");
+       
+       // for self care
+       id("selfCare-current").textContent = info(5, timeframe, "current");
+       id("selfCare-previous").textContent = info(5, timeframe, "previous");
+       
+}
+
 
 // For Daily
-id("daily").addEventListener("click", () => {
-    className(".timeframe").forEach((element) => {
-     element.textContent = "Yesterday";  
-    });
-    
-    const timeframe = "daily";
-    
-    // for work
-    id("work-current").textContent = info(0, timeframe, "current");
-    id("work-previous").textContent = info(0, timeframe, "previous");
-    
-    // for play
-    id("play-current").textContent = info(1, timeframe, "current");
-    id("play-previous").textContent = info(1, timeframe, "previous");
-    
-    // for study
-    id("study-current").textContent = info(2, timeframe, "current");
-    id("study-previous").textContent = info(2, timeframe, "previous");
-    
-    // for exercise
-    id("exercise-current").textContent = info(3, timeframe, "current");
-    id("exercise-previous").textContent = info(3, timeframe, "previous");;
-    
-    // for social
-    id("social-current").textContent = info(4, timeframe, "current");
-    id("social-previous").textContent = info(4, timeframe, "previous");
-    
-    // for self care
-    id("selfCare-current").textContent = info(5, timeframe, "current");
-    id("selfCare-previous").textContent = info(5, timeframe, "previous");
-    
-});
+id("daily").addEventListener("click", () => changeData("daily", "Yesterday"));
     
 
 // For Weekly
-id("weekly").addEventListener("click", () => {
-    className(".timeframe").forEach((element) => {
-     element.textContent = "Last Week";  
-    });
+id("weekly").addEventListener("click", () => changeData("weekly", "Last Week"));
     
-    const timeframe = "weekly";
-    
-    // for work
-    id("work-current").textContent = info(0, timeframe, "current");
-    id("work-previous").textContent = info(0, timeframe, "previous");
-    
-    // for play
-    id("play-current").textContent = info(1, timeframe, "current");
-    id("play-previous").textContent = info(1, timeframe, "previous");
-    
-    // for study
-    id("study-current").textContent = info(2, timeframe, "current");
-    id("study-previous").textContent = info(2, timeframe, "previous");
-    
-    // for exercise
-    id("exercise-current").textContent = info(3, timeframe, "current");
-    id("exercise-previous").textContent = info(3, timeframe, "previous");;
-    
-    // for social
-    id("social-current").textContent = info(4, timeframe, "current");
-    id("social-previous").textContent = info(4, timeframe, "previous");
-    
-    // for self care
-    id("selfCare-current").textContent = info(5, timeframe, "current");
-    id("selfCare-previous").textContent = info(5, timeframe, "previous");
-    
-});
-
 
 // For Monthly
-id("monthly").addEventListener("click", () => {
-    className(".timeframe").forEach((element) => {
-     element.textContent = "Last Month";  
-    });
+id("monthly").addEventListener("click", () => changeData("monthly", "Last Month"));
     
-    const timeframe = "monthly";
-    
-    // for work
-    id("work-current").textContent = info(0, timeframe, "current");
-    id("work-previous").textContent = info(0, timeframe, "previous");
-    
-    // for play
-    id("play-current").textContent = info(1, timeframe, "current");
-    id("play-previous").textContent = info(1, timeframe, "previous");
-    
-    // for study
-    id("study-current").textContent = info(2, timeframe, "current");
-    id("study-previous").textContent = info(2, timeframe, "previous");
-    
-    // for exercise
-    id("exercise-current").textContent = info(3, timeframe, "current");
-    id("exercise-previous").textContent = info(3, timeframe, "previous");;
-    
-    // for social
-    id("social-current").textContent = info(4, timeframe, "current");
-    id("social-previous").textContent = info(4, timeframe, "previous");
-    
-    // for self care
-    id("selfCare-current").textContent = info(5, timeframe, "current");
-    id("selfCare-previous").textContent = info(5, timeframe, "previous");
-    
-});
